1ea993e14fba7fea76bb03d0d93c711d860bc40d
GrayNguyen <109534682+GrayNguyen@users.noreply.github.com>
2025-01-07 13:09:39 +0700

    feat/new-air-producer

--- producer-air/producer-air.py
+++ producer-air/producer-air.py
@@ -22,21 +22,21 @@ def connect_to_db():
     return air_collection
 
 # Real-time Air Quality key
-access_token = os.getenv('AQICN_ACCESS_TOKEN')
+access_token = os.getenv('WEATHER_API_ACCESS_TOKEN')
 
 # Connect to the Real-time Air Quality data feed
-base_url = "https://api.waqi.info/feed/"
+base_url = "http://api.weatherapi.com/v1/"
 
 async def get_air(city):
     """Fetch the current air data from Real-time Air Quality data feed."""
     async with aiohttp.ClientSession() as session:
-        url = f"{base_url}{city}/?token={access_token}"
+        url = f"{base_url}current.json?key={access_token}&q=${city}&aqi=yes"
         async with session.get(url) as response:
             if response.status == 200:
                 air_data = await response.json()
                 return air_data
             else:
-                print(f"Error: {response.status} - {response.text()}")
+                print(f"Error: {response.status} - {await response.text()}")
                 return None
 
 def insert_to_mongo(air_collection, air_data):
@@ -50,24 +50,17 @@ def insert_to_mongo(air_collection, air_data):
 
 async def run(air_collection):
     """Main function to fetch and store air data in a loop."""
-    cities = ["ho-chi-minh-city", "danang", "hanoi"]
-    cities = [
-        {"location": "Ho Chi Minh City", "url_city": "ho-chi-minh-city"},
-        {"location": "Da Nang", "url_city": "danang"},
-        {"location": "Ha Noi", "url_city": "hanoi"},
-    ]
+    cities = ["Ho Chi Minh", "Da Nang","Ha Noi"]
     iterator = 0
     cooldown = 15
 
     while True:
         for city in cities:
-            response = await get_air(city["url_city"])
+            response = await get_air(city)
             if response:
                 # Add additional data like the location name and timestamp
-                air_data = response["data"]
+                air_data = response
                 air_data["report_time"] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                air_data["city"]["location"]=city["location"]
-                print(air_data)
 
                 # Insert the data into MongoDB
                 insert_to_mongo(air_collection, air_data)
